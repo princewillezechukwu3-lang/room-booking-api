c6464b5b0f5363b04e46d71dcd900de72c0da973
Ezechukwu Princewill <princewillezechukwu3@gmail.com>
2026-07-06 04:11:53 +0100

    correct min_role_requirement typo in room controller validation

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -2,13 +2,13 @@ const roomModel = require('../models/roomModel')
 
 const createRoom = async(req, res, next) => {
     try{
-        const {name, hourly_rate, min_role_reqirement} = req.body
-        if(!name || !hourly_rate || !min_role_reqirement){
+        const {name, hourly_rate, min_role_requirement} = req.body
+        if(!name || !hourly_rate || !min_role_requirement){
             const err = new Error('Missing Input')
             err.statusCode=400
             return next(err)
         }
-        const createR = await roomModel.createRoom(name, hourly_rate, min_role_reqirement)
+        const createR = await roomModel.createRoom(name, hourly_rate, min_role_requirement)
         res.status(201).json(createR)
     } catch(error){
         next(error)
